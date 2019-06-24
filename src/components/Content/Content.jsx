ec6ec39ee27d64bcb7da8093aec95b4fede37598
KirilMats <mfkirill7@gmail.com>
2019-06-25 00:38:24 +0300

    created usersContainer, usersReducer and conneted users component to store through container

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import c from './Content.module.css';
 import Profile from './Profile/Profile';
-<<<<<<< HEAD
-import UsersContainer from './Users/UsersContainer';
-=======
->>>>>>> 24e2f29ffa1b956c6d681ee70d6e5fe7e38dc34c
 import MessagesContainer from './Messages/MessagesContainer';
 import Feed from './Feed/Feed';
 import Users from './Users/Users';
@@ -12,20 +8,13 @@ import Music from './Music/Music';
 import Settings from './Settings/Settings';
 import { Route } from 'react-router-dom';
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 24e2f29ffa1b956c6d681ee70d6e5fe7e38dc34c
 const Content = () => {
   return (
     <main className={c.content}>
       <img src="https://luminous-landscape.com/wp-content/uploads/2012/11/LL_Landscape_11-12-12.jpg" />
       <Route render={ () => <Profile />} path='/profile' />
-<<<<<<< HEAD
-      <Route render={ () => <UsersContainer />} path='/users' />
-=======
       <Route render={ () => <Users />} path='/users' />
->>>>>>> 24e2f29ffa1b956c6d681ee70d6e5fe7e38dc34c
       <Route render={ () => <MessagesContainer />} path='/dialogs' />
       <Route render={ () => <Feed />} path='/feed' />
       <Route render={ () => <Music />} path='/music' />
--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -4,11 +4,7 @@ import c from './Users.module.css';
 const Users = (props) => {
     return (
         <div className={c.users}>
-<<<<<<< HEAD
-            {props.users}
-=======
             USERS
->>>>>>> 24e2f29ffa1b956c6d681ee70d6e5fe7e38dc34c
         </div>
     )
 } 
