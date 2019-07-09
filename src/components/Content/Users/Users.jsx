ffbafa63cb144a6e3c16e71bbdfa65107bd4f182
KirilMats <mfkirill7@gmail.com>
2019-07-09 19:12:01 +0300

    Moved API logic to UsersContainer and left Users.jsx as pure function

--- src/components/Content/Users/Users.jsx
+++ src/components/Content/Users/Users.jsx
@@ -1,69 +1,28 @@
 import React from 'react';
 import c from './Users.module.css';
 import User from './User/User';
-import * as axios from 'axios';
 import defaultUserPhoto from '../../../assets/img/default-user.png';
 
-// const Users = (props) => {
-//     if(props.users.length === 0){
-//         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-//             props.setUsers(response.data.items);  
-//         });
-//         // props.setUsers([
-//         //     { id: 1, isFollowed: true, name: 'Jon', status: 'wdwef1f2f', location: {city: 'Dragstone', country: 'Westeros'}, img_src: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/jon-snow-rhaegar-targaryen-1554321615.jpg?crop=0.482xw:0.962xh;0,0&resize=480:*' },
-//         //     { id: 2, isFollowed: true, name: 'Sansa', status: '1212!f1 fewwef ', location: {city: 'Winterfell', country: 'Westeros'}, img_src: 'https://s3.r29static.com//bin/entry/4ca/720x864,85/2160612/youre-probably-going-to-need-2160612.webp' },
-//         //     { id: 3, isFollowed: false, name: 'Cersei', status: '21e 12 21e 21', city: 'Kyiv', location: {city: 'Casterly Rock', country: 'Westeros'}, img_src: 'https://pixel.nymag.com/imgs/fashion/daily/2019/04/29/29-cersei.w700.h700.jpg' },
-//         //     { id: 4, isFollowed: true, name: 'Daenerys', status: '21e 12 21e 21', city: 'Kyiv', location: {city: 'Dragstone', country: 'Westeros'}, img_src: 'https://www.telegraph.co.uk/content/dam/fashion/2017/06/19/TELEMMGLPICT000131421802_trans_NvBQzQNjv4BqkUE_BTgBOQu3VWKvpDGX9fr7sARQy7EgBjwPUzvqL_M.jpeg?imwidth=450' },
-//         //     { id: 5, isFollowed: false, name: 'Jaime', status: '211e 12 21e 21', city: 'Kyiv', location: {city: 'Casterly Rock', country: 'Westeros'}, img_src: 'https://www.playerprofiler.com/wp-content/uploads/2017/07/Jaime-Lannister-1.png'} 
-//         // ]);
-//     }
-//     return (
-//         <div className={c.users}>
-//             {
-//                 props.users.map( (u) => <User key={u.id} id={u.id} isFollowed={u.followed} name={u.name} status={u.status} img_src={u.photos.small != null ? u.photos.small : defaultUserPhoto} submitFollowing={props.submitFollowing} submitUnfollowing={props.submitUnfollowing} />)
-//             }
-//         </div>
-//     )
-// }
-class Users extends React.Component {
-    // constructor(props) { // если в компоненте конструктор только лишь вызывает родительский конструктор, то можно его вообще не определять
-    //     super(props); // вызывается конструктор родительский (от React.Component)
-    // }
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-            this.props.setUsersTotalCount(response.data.totalCount);
-        });
-    }
-    onPageChange = (page) => {
-        this.props.setCurrentPage(page)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items);
-        });
-    }
-    render() {
-
-        let pagesCount = Math.ceil(this.props.usersTotalCount / this.props.pageSize);
+const Users = (props) => {
+    let pagesCount = Math.ceil(props.usersTotalCount / props.pageSize);
         
-        let pages = [];
+    let pages = [];
 
-        for(let i = 1; i <= pagesCount; i++){
-            pages.push(i);
-        }
-
-        return (
-            <div className={c.users}>
-                <div className={c.pages}>
-                    {
-                       pages.map( p => <span onClick={(e) => {this.onPageChange(p)} } className={this.props.currentPage === p && c.activePage}>{p}</span>) 
-                    }
-                </div>
+    for(let i = 1; i <= pagesCount; i++){
+        pages.push(i);
+    }
+    return (
+        <div className={c.users}>
+            <div className={c.pages}>
                 {
-                    this.props.users.map( (u) => <User key={u.id} id={u.id} isFollowed={u.followed} name={u.name} status={u.status} img_src={u.photos.small != null ? u.photos.small : defaultUserPhoto} submitFollowing={this.props.submitFollowing} submitUnfollowing={this.props.submitUnfollowing} />)
+                    pages.map( p => <span onClick={(e) => {props.onPageChange(p)} } className={props.currentPage === p && c.activePage}>{p}</span>) 
                 }
             </div>
-        )
-    }
+            {
+                props.users.map( (u) => <User key={u.id} id={u.id} isFollowed={u.followed} name={u.name} status={u.status} img_src={u.photos.small != null ? u.photos.small : defaultUserPhoto} submitFollowing={props.submitFollowing} submitUnfollowing={props.submitUnfollowing} />)
+            }
+        </div>
+    )
 }
 
 export default Users;
--- src/components/Content/Users/UsersContainer.jsx
+++ src/components/Content/Users/UsersContainer.jsx
@@ -1,6 +1,29 @@
-import Users from './Users';
+import React from 'react';
 import {connect} from 'react-redux';
 import {submitFollowingAC, setUsersAC, setUsersTotalCountAC, setCurrentPageAC} from './../../../redux/users-reducer';
+import * as axios from 'axios';
+import Users from './Users';
+
+class UsersContainer extends React.Component {
+    // constructor(props) { // если в компоненте конструктор только лишь вызывает родительский конструктор, то можно его вообще не определять
+    //     super(props); // вызывается конструктор родительский (от React.Component)
+    // }
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+            this.props.setUsersTotalCount(response.data.totalCount);
+        });
+    }
+    onPageChange = (page) => {
+        this.props.setCurrentPage(page)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }
+    render() {
+        return <Users submitUnfollowing={this.props.submitUnfollowing} submitFollowing={this.props.submitFollowing} currentPage={this.props.currentPage} usersTotalCount={this.props.usersTotalCount} pageSize={this.props.pageSize} onPageChange={this.onPageChange} users={this.props.users} />
+    }
+}
 
 const mapStateToProps = (state) => {
     return {
@@ -28,6 +51,4 @@ const mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
