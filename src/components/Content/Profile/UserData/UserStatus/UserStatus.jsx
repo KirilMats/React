d6b848cd70335b625d0dcc5d791ef2d718785e99
KirilMats <mfkirill7@gmail.com>
2019-09-24 10:55:38 -0500

    continue working on Status

--- src/components/Content/Profile/UserData/UserStatus/UserStatus.jsx
+++ src/components/Content/Profile/UserData/UserStatus/UserStatus.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import c from './UserStatus.module.css';
 
 class UserStatus extends React.Component {
+    state = {
+        editMode: true
+    }
     render() {
         return (
             <div className={c.status_wrap}>
-                <span className={c.status}>{this.props.status}</span>
-                <input type="text" value={this.props.status} />
+                {this.props.editMode ? <input type="text" value={this.props.status} /> : <span className={c.status}>{this.props.status}</span>}
             </div>
         )
     }
